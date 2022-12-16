3375086d22d9fe55578801c3057794f920384308
Sura Atta <96949252+SuraAtta@users.noreply.github.com>
2022-12-16 16:32:52 +0300

    third

--- MyStore/admin.py
+++ MyStore/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from MyStore.models import Category ,Color,Product,ProductImage
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Color)
+
+
+class ProductImageAdmin(admin.StackedInline):
+    model = ProductImage
+    extra = 1
+
+
+class ProductAdmin(admin.ModelAdmin):
+    # def get_colors(self, obj):
+    #     return ", ".join([c.name for c in obj.colors.all()])
+
+    # get_colors.__name__ = "الالوان"
+
+    inlines = [ProductImageAdmin]
+    list_display = ["name", "category", "price","stock", "is_available", "show_hide", "created", "updated"]
+    list_editable = ["is_available", "show_hide"]
+    search_fields = ['name']
+
+
+admin.site.register(Product, ProductAdmin)
+
